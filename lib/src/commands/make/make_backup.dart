a57b8e2b01b14efe2cf3d86334f0a5455aa3861c
Markus Krebs <markuskrebs93@gmail.com>
2021-03-15 21:48:25 +0100

    fix: restic command

--- lib/src/commands/make/make_backup.dart
+++ lib/src/commands/make/make_backup.dart
@@ -113,13 +113,13 @@ class MakeBackup extends Command<dynamic> {
   //
   void makeResticCall(String pathToCompressedFile) {
     // todo start restic client with compressed file and diagnostic output
-    'restic -r rest:${_store!.resticServerPath} --no-cache --verbose backup ${'pwd'.firstLine}/$backupDir'
+    'restic -r rest:${_store!.resticServerPath} backup ${'pwd'.firstLine}/$backupDir'
         .forEach((line) {
       print('$tag restic command \n $line');
     }, stderr: _stopAndMakeErrorReport, runInShell: true);
 
     // clean local backup repository afterwards
-    'restic prune'.start(runInShell: true);
+    //'restic prune'.start(runInShell: true);
   }
 
   void _parseInput() {
